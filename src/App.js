ba216f75ee13fc014e11f3fb5999e40223971a4b
chakri-hash <69791496+chakri-hash@users.noreply.github.com>
2020-12-21 21:01:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Details from './Details';
+
+function App() {
+  return (
+    <div>
+      < Details name="abcd" clg="123 clg"/>
+      < Details name="efgh" clg="456 clg"/>
+      < Details name="ijkl" clg="789 clg"/>
+
+    </div>
+  );
+}
+
+export default App
---
+++ src/Details.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Details(props) {
+    return (
+        <div>
+            Hey hai i am {props.name}. I am a student in {props.clg} 
+        </div>
+    );
+}
+
+export default Details
